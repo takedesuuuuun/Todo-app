706c1f435fd7e2d00ab943f4d01d60c0b4d48226
takedesuuuuun <takedesuuuuun@gmail.com>
2026-09-15 14:49:21 +0900

    footer baseline

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,8 +1,31 @@
+import "./footer.css"
+
 export function Footer() {
 
   return(
     <footer>
       <p>takedesuuuuun</p>
+      
+      <div className="link">
+        <a
+          href="https://github.com/takedesuuuuun/Todo-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <img src="../public/GitHub_Invertocat_Black.png" alt="GitHub" />
+        </a>
+
+        <a
+          href="https://portfolio-takedesuuuuun.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Portfolio"
+        >
+          Portfolio
+        </a>
+      </div>
+      
       <p><small>&copy; 2026 takedesuuuuun</small></p>
     </footer>
   );
